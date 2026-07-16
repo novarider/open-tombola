be636d5f5cf4323cd73822c5c2831af8be9d1ba9
Patrick Rudigier <pat.rudigier@gmail.com>
2026-07-16 15:27:32 +0000

    documented how to create sql function to create tombola results

--- libs/api/products/src/lib/API/TombolaController.ts
+++ libs/api/products/src/lib/API/TombolaController.ts
@@ -29,6 +29,65 @@ export class TombolaController {
          *      select orderid, firstname, lastname, weight, createdat, checkoutdoneat from 
          *          (select checkouts.fk_orderid, weight, checkoutdoneat from 
          *                  left join tickets on checkouts.fk_orderid = tickets.fk_orderid) as t1 left join orders on t1.fk_orderid = orders.orderid;
+         * 
+         * Get all payed checkouts
+         *  select * from checkouts where checkoutdoneat IS NOT NULL and (paymentreference = 'offline' OR (paymentreference <> '' AND checkoutstatus = 'complete'));
+         * 
+         * Get all order ids sorted by absolute difference to weight
+         *      SELECT t1.fk_orderid, weight, ABS(weight - 334.5) AS diff FROM 
+         *          (SELECT fk_orderid FROM checkouts WHERE 
+         *              checkoutdoneat IS NOT NULL AND 
+         *              (paymentreference = 'offline' OR (paymentreference <> '' AND checkoutstatus = 'complete')
+         *          )
+         *      ) AS t1 LEFT JOIN tickets 
+         *          ON t1.fk_orderid = tickets.fk_orderid 
+         *          ORDER BY diff ASC;
+         * 
+         * Get calculated nearest ticket tips with order owner and address
+         *      SELECT t2.diff, t2.weight, orders.firstname, orders.lastname, orders.street, orders.postalcode, orders.city, orders.country FROM
+         *          (SELECT t1.fk_orderid, weight, ABS(weight - 3) AS diff FROM 
+         *              (SELECT fk_orderid FROM checkouts WHERE 
+         *                  checkoutdoneat IS NOT NULL AND 
+         *                  (paymentreference = 'offline' OR (paymentreference <> '' AND checkoutstatus = 'complete')
+         *              )
+         *              ) AS t1 LEFT JOIN tickets 
+         *              ON t1.fk_orderid = tickets.fk_orderid) AS t2 
+         *                  LEFT JOIN orders 
+         *                  ON t2.fk_orderid = orders.orderid 
+         *                  ORDER BY diff ASC;
+         * 
+         * Function to do tombola result calculation
+          
+           CREATE OR REPLACE FUNCTION tombola_result(
+                winningWeight real
+                )
+                RETURNS TABLE(
+                    difference real,
+                    guessed_weight real,
+                    firstname varchar(50),
+                    lastname varchar(50),
+                    street varchar(50),
+                    postalcode varchar(50),
+                    city varchar(50),
+                    country varchar(50)
+                )
+                AS $$
+                BEGIN
+                RETURN QUERY
+                -- SQL_statements to be executed
+                    SELECT t2.diff, t2.weight, orders.firstname, orders.lastname, orders.street, orders.postalcode, orders.city, orders.country FROM
+                        (SELECT t1.fk_orderid, weight, ABS(weight - @winningWeight) AS diff FROM 
+                            (SELECT fk_orderid FROM checkouts WHERE 
+                                checkoutdoneat IS NOT NULL AND 
+                                (paymentreference = 'offline' OR (paymentreference <> '' AND checkoutstatus = 'complete')
+                            )
+                            ) AS t1 LEFT JOIN tickets 
+                            ON t1.fk_orderid = tickets.fk_orderid) AS t2 
+                                LEFT JOIN orders 
+                                ON t2.fk_orderid = orders.orderid 
+                                ORDER BY diff ASC;
+                END;$$
+                LANGUAGE plpgsql;
          */
         this.app.get('/tombola/tickets', async (req, res) => {
             try {
